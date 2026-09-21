634e2e0b0016b924d493226e3d925150b0ea4006
salamsalmansalmon <salammuhammadfarhan@gmail.com>
2026-09-21 16:40:41 +0700

    Update and rename main.py to oldmain.py

--- oldmain.py
+++ oldmain.py
@@ -3,7 +3,20 @@ from typing import Final
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import os
+import html
+from typing import Optional, List
 
+from telegram import Message, Chat, Update, Bot, User
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, Filters
+from telegram.ext.dispatcher import run_async
+from telegram.utils.helpers import mention_html
+
+from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
+from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from tg_bot.modules.helper_funcs.string_handling import extract_time
+from tg_bot.modules.log_channel import loggable
 TOKEN = os.getenv("BOT_TOKEN")
 
 if TOKEN is None:
